9e44a4fc09050755303078b96f14e95da29eb0f7
Nitushkakitke <kitkenitushka@gmail.com>
2025-01-18 15:41:14 +0530

    msg

---
+++ bootstrap/javascript/index.js
@@ -0,0 +1,13 @@
+console.log("Welcome to javascript code");
+console.log("This is console statement");
+//document.write not supported by .js extension
+let a=20, b=10;
+if(a>b){
+    console.log("a is greater");
+}
+else{
+    console.log("b is greater")
+}
+for(var i=1;i<=10;i++){
+    console.log(5 +"*"+i+"="+5*i);
+}
